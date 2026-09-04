0e4dc5391128e2e7d51b5e2aa54ad3c5be75c05d
Trilicia Gomes <triliciamgomes@gmail.com>
2026-09-04 14:36:06 -0300

    Etapa 1

---
+++ Atividade_banco.sql
@@ -0,0 +1,13 @@
+/*
+ETAPA 1 — ENTIDADES
+
+As entidades identificadas são:
+
+1. alunos
+2. cursos
+3. matriculas
+
+A tabela matriculas conecta alunos e cursos, resolvendo o
+relacionamento muitos para muitos existente entre eles.
+*/
+
